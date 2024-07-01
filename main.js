0f6b9c9ddddd92f760fbc11e01d7a9e45bb0c643
pablo francone <166070187+PabloFrancone@users.noreply.github.com>
2024-07-01 02:58:44 -0300

    NasheMan(funciona por cristo)

--- main.js
+++ main.js
@@ -3,24 +3,24 @@ import Game from "./scenes/Game.js";
 // Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
-  width: 2500,
-  height: 2000,
+  width: 1500,
+  height: 1000,
   scale: {
     mode: Phaser.Scale.FIT,
     
     min: {
       width: 1000,
-      height: 1000,
+      height: 800,
     },
     max: {
       width: 2600,
-      height: 2200,
+      height: 800,
     },
   },
   physics: {
     default: "arcade",
     arcade: {
-      debug: true
+      debug: false
     },
   },
 
--- scenes/Game.js
+++ scenes/Game.js
@@ -17,39 +17,17 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    // Crear el mapa inicial
+    // Configurar variables de estado inicial
     this.ultimoEstado = "";
     this.maps = 0;
-    this.mapaActual = this.make.tilemap({ key: "Proyecto" });
-    const tiles = this.mapaActual.addTilesetImage("escenario", "espacio");
 
-    this.LayerAbajo = this.mapaActual.createLayer("piso", tiles, 0, 0);
-    this.LayerArriba = this.mapaActual.createLayer("paredes", tiles, 0, 0);
-    this.LayerArriba.setCollisionByProperty({ coliders: true });
-
-    // Crear el robot
-    this.robot = this.physics.add.sprite(628, 428, "robot").setScale(0.2);
-    this.robot.setDepth(1);
-    this.robot.setSize(this.robot.width * 0.5, this.robot.height * 0.7);
-    this.physics.add.collider(this.robot, this.LayerArriba);
-
-    // Crear grupos para objetos
-    this.objetosNoDestruibles = this.physics.add.staticGroup();
-    this.objetosDestruibles = this.physics.add.group();
-
-    // Generar objetos aleatorios al iniciar
-    this.generarObjetosAleatorios();
-
-    // Configurar colisiones
-    this.physics.add.collider(this.robot, this.objetosNoDestruibles);
-    this.physics.add.collider(this.robot, this.objetosDestruibles, this.destruirObjeto, null, this);
+    // Crear el mapa inicial
+    this.generarMapaInicial();
 
     // Configurar la cámara
     this.mycam = this.cameras.main;
     this.mycam.startFollow(this.robot);
-
-    // Renderizar gráficos de depuración de colisiones si es necesario
-    this.renderizarGraficosDepuracion(this.LayerArriba);
+    this.mycam.centerOn(this.robot.x, this.robot.y);
 
     // Configurar animaciones del robot
     this.configurarAnimacionesRobot();
@@ -71,16 +49,25 @@ export default class Game extends Phaser.Scene {
     // Manejar el movimiento del robot
     if (this.cursors.left.isDown) {
       this.moverRobot(-velocidad, 0, "right", "robot-run-side(right)");
+      this.ultimoEstado = "right";
     } else if (this.cursors.right.isDown) {
       this.moverRobot(velocidad, 0, "left", "robot-run-side(left)");
+      this.ultimoEstado = "left";
     } else if (this.cursors.up.isDown) {
       this.moverRobot(0, -velocidad, "up", "robot-run-up");
+      this.ultimoEstado = "up";
     } else if (this.cursors.down.isDown) {
       this.moverRobot(0, velocidad, "below", "robot-run-below");
+      this.ultimoEstado = "below";
     } else {
-      this.robot.anims.play(`robot-idle-${this.ultimoEstado}`);
+      this.robot.anims.play("robot-run-below");
       this.robot.setVelocity(0, 0);
     }
+
+    // Actualizar la posición de los enemigos para que persigan al robot
+    this.enemigos.children.iterate((enemigo) => {
+      this.physics.moveToObject(enemigo, this.robot, 200);
+    });
 
     // Verificar colisiones con la capa de arriba del mapa actual
     this.physics.world.collide(this.robot, this.LayerArriba);
@@ -91,27 +78,135 @@ export default class Game extends Phaser.Scene {
     }
   }
 
-  moverRobot(velocidadX, velocidadY, estado, animacion) {
-    this.robot.setVelocity(velocidadX, velocidadY);
+  generarMapaInicial() {
+    // Crear el mapa inicial
+    this.mapaActual = this.make.tilemap({ key: "Proyecto" });
+    const tiles = this.mapaActual.addTilesetImage("escenario", "espacio");
+
+    this.LayerAbajo = this.mapaActual.createLayer("piso", tiles, 0, 0);
+    this.LayerArriba = this.mapaActual.createLayer("paredes", tiles, 0, 0);
+    this.LayerArriba.setCollisionByProperty({ coliders: true });
+
+    // Crear el robot
+    this.robot = this.physics.add.sprite(628, 428, "robot").setScale(0.2);
+    this.robot.setDepth(1);
+    this.robot.setSize(this.robot.width * 0.5, this.robot.height * 0.7);
+    this.physics.add.collider(this.robot, this.LayerArriba);
+
+    // Crear grupos para objetos
+    this.objetosNoDestruibles = this.physics.add.staticGroup();
+    this.objetosDestruibles = this.physics.add.group();
+
+    // Crear grupo para enemigos
+    this.enemigos = this.physics.add.group();
+
+    // Generar objetos y enemigos aleatorios al iniciar
+    this.generarObjetosAleatorios();
+    this.generarEnemigosAleatorios();
+
+    // Configurar colisiones entre robot y objetos
+    this.physics.add.collider(this.robot, this.objetosNoDestruibles);
+    this.physics.add.collider(this.robot, this.objetosDestruibles, this.destruirObjeto, null, this);
+
+    // Configurar colisiones entre robot y enemigos
+    this.physics.add.collider(this.robot, this.enemigos, this.colisionEnemigo, null, this);
+  }
+
+  generarSiguienteMapa() {
+    // Generar el siguiente mapa y mover el actual hacia abajo
+    this.maps++;
+    this.mapaActual = this.make.tilemap({ key: "MapaGenerado" });
+    const tiles = this.mapaActual.addTilesetImage("escenario", "espacio");
+
+    const nuevaLayerAbajo = this.mapaActual.createLayer(
+      "piso",
+      tiles,
+      0,
+      this.maps * this.mapaActual.heightInPixels
+    );
+    const nuevaLayerArriba = this.mapaActual.createLayer(
+      "paredes",
+      tiles,
+      0,
+      this.maps * this.mapaActual.heightInPixels
+    );
+
+    nuevaLayerArriba.setCollisionByProperty({ coliders: true });
+    this.physics.add.collider(this.robot, nuevaLayerArriba);
+
+    // Actualizar las capas actuales
+    this.LayerAbajo = nuevaLayerAbajo;
+    this.LayerArriba = nuevaLayerArriba;
+
+    // Renderizar gráficos de depuración para colisiones
+    this.renderizarGraficosDepuracion(this.LayerArriba);
+
+    // Generar nuevos objetos y enemigos aleatorios
+    this.generarObjetosAleatorios();
+    this.generarEnemigosAleatorios();
+  }
+
+  generarObjetosAleatorios() {
+    // Generar nuevos objetos
+    const cantidadObjetos = 10; // Cantidad de objetos a generar
+    const spawnAreaWidth = this.LayerAbajo.width; // Ancho del área de generación
+    const offsetY = this.maps * this.LayerAbajo.height; // Desplazamiento en el eje Y según el mapa
+
+    for (let i = 0; i < cantidadObjetos; i++) {
+      // Generar posición aleatoria dentro del área de juego
+      const x = Phaser.Math.Between(200, spawnAreaWidth - 200);
+      const y = Phaser.Math.Between(300, this.LayerAbajo.height - 300) + offsetY;
+
+      // Elegir aleatoriamente entre objeto destruible o no destruible
+      if (Phaser.Math.Between(0, 1) === 0) {
+        // Objeto no destruible
+        const objetoNoDestruible = this.objetosNoDestruibles.create(x, y, "objeto-no");
+        objetoNoDestruible.setSize(objetoNoDestruible.width * 0.1, objetoNoDestruible.height * 0.18).setScale(0.2).setDepth(2);
+      } else {
+        // Objeto destruible
+        const objetoDestruible = this.objetosDestruibles.create(x, y, "objeto-si").setScale(0.2);
+        objetoDestruible.setSize(objetoDestruible.width * 0.7, objetoDestruible.height * 0.8).setDepth(2);
+      }
+    }
+  }
+
+  generarEnemigosAleatorios() {
+    // Verificar si ya hay 5 enemigos activos
+    if (this.enemigos.countActive(true) >= 5) {
+        return; // Salir del método si ya hay 5 enemigos activos
+    }
+
+    // Configurar parámetros de generación
+    const spawnAreaWidth = this.LayerAbajo.width; // Ancho del área de generación
+    const spawnAreaHeight = this.LayerAbajo.height; // Alto del área de generación
+    const offsetY = this.maps * this.LayerAbajo.height; // Desplazamiento en el eje Y según el mapa
+
+    // Generar enemigos
+    const x = Phaser.Math.Between(200, spawnAreaWidth - 200);
+    const y = Phaser.Math.Between(300, spawnAreaHeight - 300) + offsetY;
+
+    // Añadir un retraso de 2 segundos antes de generar el enemigo
+    this.time.delayedCall(2000, () => {
+        const enemigo = this.enemigos.create(x, y, "enemigo").setScale(0.05);
+        enemigo.setSize(enemigo.width * 0.3, enemigo.height * 0.7);
+        enemigo.setDepth(2);
+
+        // Configurar comportamiento de colisiones y físicas para enemigos
+        this.physics.add.collider(enemigo, this.LayerArriba);
+        this.physics.add.collider(this.robot, enemigo, this.colisionEnemigo, null, this);
+    });
+}
+
+
+  moverRobot(velX, velY, estado, animacion) {
+    this.robot.setVelocity(velX, velY);
     this.robot.anims.play(animacion, true);
     this.ultimoEstado = estado;
   }
 
   configurarAnimacionesRobot() {
     // Definir las animaciones del robot
-    const animaciones = [
-      { key: "robot-idle-left", frame: ".A&Mu-robot-run-left-00.png" },
-      { key: "robot-idle-right", frame: ".A&Mu-robot-run-right-00.png" },
-      { key: "robot-idle-below", frame: ".A&Mu-robot-run-below-00.png" },
-      { key: "robot-idle-up", frame: ".A&Mu-robot-run-up-00.png" },
-    ];
-
-    animaciones.forEach(animacion => {
-      this.anims.create({
-        key: animacion.key,
-        frames: [{ key: "robot", frame: animacion.frame }],
-      });
-    });
+    
 
     this.anims.create({
       key: "robot-run-side(left)",
@@ -151,76 +246,19 @@ export default class Game extends Phaser.Scene {
   }
 
   renderizarGraficosDepuracion(layer) {
-    const debugGraphics = this.add.graphics().setAlpha(0.75);
-    layer.renderDebug(debugGraphics, {
-      tileColor: null,
-      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255),
-    });
-  }
-
-  generarSiguienteMapa() {
-    // Generar el siguiente mapa y mover el actual hacia abajo
-    this.maps++;
-    this.mapaActual = this.make.tilemap({ key: "MapaGenerado" });
-    const tiles = this.mapaActual.addTilesetImage("escenario", "espacio");
-
-    const nuevaLayerAbajo = this.mapaActual.createLayer(
-      "piso",
-      tiles,
-      0,
-      this.maps * this.mapaActual.heightInPixels
-    );
-    const nuevaLayerArriba = this.mapaActual.createLayer(
-      "paredes",
-      tiles,
-      0,
-      this.maps * this.mapaActual.heightInPixels
-    );
-
-    nuevaLayerArriba.setCollisionByProperty({ coliders: true });
-    this.physics.add.collider(this.robot, nuevaLayerArriba);
-
-    // Actualizar las capas actuales
-    this.LayerAbajo = nuevaLayerAbajo;
-    this.LayerArriba = nuevaLayerArriba;
-
-    // Renderizar gráficos de depuración para colisiones
-    this.renderizarGraficosDepuracion(this.LayerArriba);
-
-    // Generar nuevos objetos aleatorios
-    this.generarObjetosAleatorios();
-  }
-
-  generarObjetosAleatorios() {
-    // Eliminar objetos existentes
-    
-
-    // Generar nuevos objetos
-    const cantidadObjetos = 10; // Cantidad de objetos a generar
-    const spawnAreaWidth = this.LayerAbajo.width; // Ancho del área de generación
-    const offsetY = this.maps * this.LayerAbajo.height; // Desplazamiento en el eje Y según el mapa
-
-    for (let i = 0; i < cantidadObjetos; i++) {
-      // Generar posición aleatoria dentro del área de juego
-      const x = Phaser.Math.Between(200, spawnAreaWidth - 200);
-      const y = Phaser.Math.Between(300, this.LayerAbajo.height - 300) + offsetY;
-
-      // Elegir aleatoriamente entre objeto destruible o no destruible
-      if (Phaser.Math.Between(0, 1) === 0) {
-        // Objeto no destruible
-        const objetoNoDestruible = this.objetosNoDestruibles.create(x, y, "objeto-no");
-        objetoNoDestruible.setSize(objetoNoDestruible.width * 0.1, objetoNoDestruible.height * 0.21).setScale(0.2).setDepth(2);
-      } else {
-        // Objeto destruible
-        const objetoDestruible = this.objetosDestruibles.create(x, y, "objeto-si").setScale();
-        objetoDestruible.setSize(objetoDestruible.width * 0.7, objetoDestruible.height * 0.8).setScale(0.2).setDepth(2);
-      }
-    }
+    // const debugGraphics = this.add.graphics().setAlpha(0.75);
+    // layer.renderDebug(debugGraphics, {
+    //   tileColor: null,
+    //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+    //   faceColor: new Phaser.Display.Color(40, 39, 37, 255),
+    // });
   }
 
   destruirObjeto(robot, objeto) {
-    // Implementar la lógica de destrucción de objetos
     objeto.destroy();
+  }
+
+  colisionEnemigo(robot, enemigo) {
+    this.scene.restart();
   }
 }
